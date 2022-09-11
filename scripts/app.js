04182c93bcb37a91ba5fcf8774165165fc7c4343
YiiSauce <youssefyouss321@gmail.com>
2022-09-11 01:41:36 +0100

    5.1th

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const edit1 = document.getElementById("edit1");
 const edit2 = document.getElementById("edit2");
+const confirm = document.getElementById("done");
 
 function overly(){
     document.getElementById("conover").style.display = "block";
@@ -8,13 +9,26 @@ function overly(){
 function close(){
     document.getElementById("close").style.display = "none";
 }
-function confirm(){
+function done(){
+    let dd = document.getElementById("plyrname").value;
     
-    document.getElementById("done").style.display = "none";
+    alert(dd);
+}
+
+function isAlpha(ch){
+    let i=0;
+    let c=0;
+    while(i<ch.length){
+        if ((ch[i] >= "a" && ch[i] <= "z") || (ch[i] >= "A" && ch[i] <= "Z"))
+            c++;
+        i++;
+    }
+    return c==0 ? false : true;
 }
 
 edit1.addEventListener("click",overly);
 edit2.addEventListener("click",overly);
+confirm.addEventListener("click",done);
 
 
 
